9644eca2e490f4bcb6ffcc8bdd7a63d4536bfffc
snltech <zaatry.m@gmail.com>
2022-05-15 21:49:32 +0300

    added page script and css initial files

---
+++ script.js
@@ -0,0 +1,17 @@
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function(){
+        return `${title} by ${author}, ${pages} pages, ${read}`;
+    }
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, 'not read yet');
+
+let myLibrary = [];
+
+function addBookToLibrary(bookName) {
+    return myLibrary.push(bookName);
+}
