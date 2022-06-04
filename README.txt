c54951d04ad26d72e0aafece0d285b533baf05f3
snltech <zaatry.m@gmail.com>
2022-06-04 22:06:53 +0300

    added the table, modal button and modal, still need to add overly and JS code to trigger modal on and off, added a readme for checklist

---
+++ README.txt
@@ -0,0 +1,22 @@
+The project consists of creating a library of books.
+
+The library can be in 2 shapes/forms:
+1) Table
+2) Grid with info of each book inside the Grid
+
+The library will contain the following info:
+1) Title
+2) Author
+3) Number of pages in the book
+4) Status of the book, meaning: Read / Not Read
+
+Challenges:
+1) Making the button that takes new books trigger a pop-up, requires further learning of:
+    -   Positioning
+    -   Transform properties
+    -   Overlay properties
+    -   Global box sizing setting
+2) A good revision of JS selectors and forms
+3) Adding elements to the table with a remove button in the table that can trigger the deletion of parent row
+4) Checking for duplicates in the table so that the same book does not get added twice
+
--- script.js
+++ script.js
@@ -8,10 +8,32 @@ function Book(title, author, pages, read){
     }
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, 'not read yet');
-
+let propertiesArr = ['title', 'author', 'pages', 'read'];
 let myLibrary = [];
 
 function addBookToLibrary(bookName) {
     return myLibrary.push(bookName);
 }
+
+function addBooksToTable(){
+    for(let i = 0; i < myLibrary.length; i++){
+        let row = document.createElement('tr');
+        for(let j = 0; j < propertiesArr.length; j++){
+            let cellcontent = document.createElement('td');
+            cellcontent.innerText = myLibrary[i][propertiesArr[j]];
+            row.appendChild(cellcontent);
+        }
+        document.querySelector('tbody').appendChild(row);
+    }
+}
+
+//  Books
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, 'not read yet');
+const fortyEightLaws = new Book('48 Laws of Power', 'Robert Greene', "300+", 'read half');
+
+addBookToLibrary(theHobbit);
+addBookToLibrary(fortyEightLaws);
+addBooksToTable();
+
+let newBookBtn = document.querySelectorAll('.newBook');
+newBookBtn.addEventListener('click', e => alert('check'));
